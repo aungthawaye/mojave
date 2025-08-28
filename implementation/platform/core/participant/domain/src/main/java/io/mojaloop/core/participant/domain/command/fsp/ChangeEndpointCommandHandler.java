2f3196045335dec1e91da94a52b453b0c40ac460
Aung Thaw Aye <ata.aungthawaye@gmail.com>
2025-08-28 22:19:45 +0630

    added participant admin API

---
+++ implementation/platform/core/participant/domain/src/main/java/io/mojaloop/core/participant/domain/command/fsp/ChangeEndpointCommandHandler.java
@@ -0,0 +1,66 @@
+/*-
+ * ================================================================================
+ * Mojaloop OSS
+ * --------------------------------------------------------------------------------
+ * Copyright (C) 2025 Open Source
+ * --------------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ================================================================================
+ */
+
+package io.mojaloop.core.participant.domain.command.fsp;
+
+import io.mojaloop.component.jpa.routing.annotation.Write;
+import io.mojaloop.core.participant.contract.command.fsp.ChangeEndpointCommand;
+import io.mojaloop.core.participant.domain.model.repository.FspRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ChangeEndpointCommandHandler implements ChangeEndpointCommand {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChangeEndpointCommandHandler.class);
+
+    private final FspRepository fspRepository;
+
+    public ChangeEndpointCommandHandler(FspRepository fspRepository) {
+        assert fspRepository != null;
+        this.fspRepository = fspRepository;
+    }
+
+    @Override
+    @Transactional
+    @Write
+    public Output execute(Input input) {
+
+        LOGGER.info("Executing ChangeEndpointCommand with input: {}", input);
+
+        var optFsp = this.fspRepository.findById(input.fspId());
+        if (optFsp.isEmpty()) {
+            LOGGER.warn("FSP not found for id: {}. ChangeEndpointCommand will be a no-op.", input.fspId());
+            return new Output(false);
+        }
+
+        var fsp = optFsp.get();
+
+        boolean changed = fsp.changeEndpoint(input.endpointType(), input.baseUrl());
+
+        this.fspRepository.save(fsp);
+
+        LOGGER.info("Completed ChangeEndpointCommand with input: {} -> changed={}", input, changed);
+
+        return new Output(changed);
+    }
+}
