7260acce8b07378ac8c843c7988ed58055fa8f61
Aung Thaw Aye <ata.aungthawaye@gmail.com>
2025-10-11 06:51:02 +0630

    adding Wallet module

--- implementation/platform/core/04_transaction/domain/src/main/java/io/mojaloop/core/transaction/domain/model/definition/FundTransferDefinition.java
+++ implementation/platform/core/04_transaction/domain/src/main/java/io/mojaloop/core/transaction/domain/model/definition/FundTransferDefinition.java
@@ -1,18 +1,29 @@
 package io.mojaloop.core.transaction.domain.model.definition;
 
+import io.mojaloop.component.jpa.JpaEntity;
 import io.mojaloop.component.misc.constraint.StringSizeConstraints;
+import io.mojaloop.component.misc.handy.Snowflake;
+import io.mojaloop.core.common.datatype.converter.identifier.accounting.ChartEntryIdJavaType;
 import io.mojaloop.core.common.datatype.converter.identifier.transaction.DefinitionIdJavaType;
+import io.mojaloop.core.common.datatype.converter.identifier.transaction.PostingIdJavaType;
 import io.mojaloop.core.common.datatype.enums.ActivationStatus;
 import io.mojaloop.core.common.datatype.enums.TerminationStatus;
+import io.mojaloop.core.common.datatype.enums.accounting.Side;
+import io.mojaloop.core.common.datatype.identifier.accounting.ChartEntryId;
 import io.mojaloop.core.common.datatype.identifier.transaction.DefinitionId;
+import io.mojaloop.core.common.datatype.identifier.transaction.PostingId;
+import io.mojaloop.core.transaction.contract.exception.DefinitionDescriptionTooLongException;
+import io.mojaloop.core.transaction.contract.exception.PostingAlreadyExistsException;
 import io.mojaloop.fspiop.spec.core.Currency;
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
@@ -58,7 +69,120 @@ public class FundTransferDefinition {
     @Enumerated(EnumType.STRING)
     protected TerminationStatus terminationStatus = TerminationStatus.ALIVE;
 
-    @OneToMany(mappedBy = "definition", orphanRemoval = true, cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
-    protected List<FundOutDefinition.Posting> postings = new ArrayList<>();
+    @OneToMany(mappedBy = "definition", orphanRemoval = true, cascade = {jakarta.persistence.CascadeType.ALL}, fetch = FetchType.EAGER)
+    protected List<Posting> postings = new ArrayList<>();
+
+    public enum PostingAmountType {
+
+        TRANSFER_AMOUNT,
+        PAYEE_FSP_FEE,
+        PAYEE_FSP_COMMISSION
+    }
+
+    @Getter
+    @Entity
+    @Table(name = "txn_fund_transfer_posting")
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Posting extends JpaEntity<PostingId> {
+
+        @Id
+        @JavaType(PostingIdJavaType.class)
+        @JdbcTypeCode(BIGINT)
+        protected PostingId id;
+
+        @Column(name = "for_amount_type", nullable = false, length = StringSizeConstraints.MAX_ENUM_LENGTH)
+        @Enumerated(EnumType.STRING)
+        protected PostingAmountType forAmountType;
+
+        @Column(name = "side", nullable = false, length = StringSizeConstraints.MAX_ENUM_LENGTH)
+        @Enumerated(EnumType.STRING)
+        protected Side side;
+
+        @Basic
+        @JavaType(ChartEntryIdJavaType.class)
+        @JdbcTypeCode(BIGINT)
+        @Column(name = "chart_entry_id", nullable = false, updatable = false)
+        protected ChartEntryId chartEntryId;
+
+        @Column(name = "description", length = StringSizeConstraints.MAX_DESCRIPTION_LENGTH)
+        protected String description;
+
+        @ManyToOne(fetch = FetchType.EAGER)
+        @JoinColumn(name = "definition_id", nullable = false)
+        protected FundTransferDefinition definition;
+
+        public Posting(FundTransferDefinition definition, PostingAmountType forAmountType, Side side, ChartEntryId chartEntryId, String description)
+            throws PostingAlreadyExistsException {
+
+            assert definition != null;
+            assert forAmountType != null;
+            assert side != null;
+            assert chartEntryId != null;
+
+            this.id = new PostingId(Snowflake.get().nextId());
+            this.definition = definition;
+            this.forAmountType(forAmountType).side(side).chartEntryId(chartEntryId).description(description);
+        }
+
+        public Posting chartEntryId(ChartEntryId chartEntryId) throws PostingAlreadyExistsException {
+
+            assert chartEntryId != null;
+
+            try {
+                this.definition.postings.stream().filter(p -> p.getChartEntryId().equals(chartEntryId)).findFirst().ifPresent(p -> {
+                    try {
+                        throw new PostingAlreadyExistsException();
+                    } catch (PostingAlreadyExistsException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            } catch (RuntimeException e) {
+
+                if (e.getCause() instanceof PostingAlreadyExistsException e1) {
+                    throw e1;
+                }
+            }
+
+            this.chartEntryId = chartEntryId;
+
+            return this;
+        }
+
+        public Posting description(String description) {
+
+            if (description == null) {
+                return this;
+            }
+
+            var value = description.trim();
+
+            if (value.length() > StringSizeConstraints.MAX_DESCRIPTION_LENGTH) {
+                throw new DefinitionDescriptionTooLongException();
+            }
+
+            this.description = description;
+
+            return this;
+        }
+
+        public Posting forAmountType(PostingAmountType forAmountType) {
+
+            assert forAmountType != null;
+
+            this.forAmountType = forAmountType;
+
+            return this;
+        }
+
+        public Posting side(Side side) {
+
+            assert side != null;
+
+            this.side = side;
+
+            return this;
+        }
+
+    }
 
 }
