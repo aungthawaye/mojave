984a1ab89b625b43744ea9aa79f3db63d3049582
Aung Thaw Aye <ata.aungthawaye@gmail.com>
2025-12-07 18:57:38 +0630

    remove mock class

--- implementation/platform/core/01_transaction/consumer/src/main/java/io/mojaloop/core/transaction/consumer/Something.java
+++
@@ -1,6 +0,0 @@
-package io.mojaloop.core.transaction.consumer;
-
-public interface Something {
-
-    void doSomething();
-}
--- implementation/platform/core/01_transaction/consumer/src/main/java/io/mojaloop/core/transaction/consumer/TransactionConsumerDependencies.java
+++ implementation/platform/core/01_transaction/consumer/src/main/java/io/mojaloop/core/transaction/consumer/TransactionConsumerDependencies.java
@@ -1,7 +1,6 @@
 package io.mojaloop.core.transaction.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
 
 final class TransactionConsumerDependencies
     implements TransactionConsumerConfiguration.RequiredDependencies {
@@ -11,18 +10,6 @@ final class TransactionConsumerDependencies
     public TransactionConsumerDependencies(ObjectMapper objectMapper) {
 
         this.objectMapper = objectMapper;
-    }
-
-    @Bean
-    public Something something() {
-
-        return new Something() {
-
-            @Override
-            public void doSomething() {
-
-            }
-        };
     }
 
 }
