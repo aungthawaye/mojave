de1028fd6a1268e784c2e7d484b4717bfee0cdf6
Aung Thaw Aye <ata.aungthawaye@gmail.com>
2025-12-07 18:59:00 +0630

    removed unused code

--- implementation/platform/core/01_transaction/consumer/src/main/java/io/mojaloop/core/transaction/consumer/TransactionConsumerDependencies.java
+++ implementation/platform/core/01_transaction/consumer/src/main/java/io/mojaloop/core/transaction/consumer/TransactionConsumerDependencies.java
@@ -1,15 +1,6 @@
 package io.mojaloop.core.transaction.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 final class TransactionConsumerDependencies
     implements TransactionConsumerConfiguration.RequiredDependencies {
-
-    private final ObjectMapper objectMapper;
-
-    public TransactionConsumerDependencies(ObjectMapper objectMapper) {
-
-        this.objectMapper = objectMapper;
-    }
 
 }
